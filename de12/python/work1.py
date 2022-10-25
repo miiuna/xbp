e649a76327748fbad411065fb4bd0e88aaccba01
miiuna <r202202058za@jindai.jp>
2022-10-25 16:53:04 +0900

    20221025

---
+++ de12/python/work1.py
@@ -0,0 +1,10 @@
+from pygments import highlight
+
+
+name="meme"
+#nameには""で名前を挟む必要がある。
+#waistという変数にウエストの数字を代入している。
+waist=60
+#ageという変数に年齢を代入している。　（例:age=44 年齢は44歳ですね。） 年齢は連続表示されない
+age=25
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
